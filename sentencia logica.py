af84af08e998072e2512aeeeae11a192b144318b
andreaJungg <andreajungcano@gmail.com>
2026-03-26 13:39:04 -0600

    Add files via upload
    ejercicios de sentencias logicas

---
+++ sentencia logica.py
@@ -0,0 +1,53 @@
+
+nombre = "andrea"
+apellido = "jung"
+
+if nombre == "andrea" and apellido == "jung":
+    print ("bienvenida andrea jung")
+
+nombre1 = "IMB"
+nombre2 = "PC"
+
+if nombre1 == "IMB" and nombre2 == "PC":
+     print ("colegio IMB PC")
+
+
+
+amiga1 = "Giovanna"
+amiga2 = "Andrea"
+
+if amiga1 == "Giovanna" and amiga2 == "Andrea":
+    print ("Giovanna y Andrea son amigas")
+
+
+profe = "Carlos"
+clase = "Programacion"
+
+if profe == "Carlos" and clase == "Programacion":
+    print ("Carlos es profe de Programacion")
+
+
+profe = "Carlos"
+clase = "Programacion"
+
+if profe == "Carlos" and clase == "Programacion":
+    print ("Carlos es profe de Programacion")
+
+
+producto1 = "labial"
+producto2 = "base"
+if producto1 == "labial" and producto2 == "base":
+     print ("labial y base son maquillaje")
+
+
+
+comida1 = "spaguetti"
+comida2 = "pan"
+
+if comida1 == "spaguetti" and comida2 == "pan":
+    print ("spaguetti y pan son comida")
+
+
+
+
+
